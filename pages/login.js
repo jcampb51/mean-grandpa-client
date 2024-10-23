b35f1349e0df04317846a2dad8a3dbe649a3bf3f
James Campbell <jamesrcampbell.obb@gmail.com>
2024-10-23 09:18:29 -0500

    some initial code to get things started

---
+++ pages/login.js
@@ -0,0 +1,63 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useRef } from "react";
+import { Input } from "../components/form-elements";
+import { login } from "../data/auth";
+import { useUserQuery } from "../context/userQueries";
+import { useMutation } from "@tanstack/react-query";
+
+
+export default function Login() {
+  const { setUserToken } = useUserQuery();
+  const username = useRef("");
+  const password = useRef("");
+  const router = useRouter();
+
+  const loginMutation = useMutation({
+    mutationFn: login,
+    onSuccess: (data) => {
+      if (data.token) {
+        setUserToken(data.token); // update the query state
+        router.push("/");
+      }
+    },
+  });
+
+  const submit = (e) => {
+    e.preventDefault();
+    const user = {
+      username: username.current.value,
+      password: password.current.value,
+    };
+    loginMutation.mutate(user);
+  };
+
+  return (
+    <div className="columns is-centered">
+      <div className="column is-half">
+        <form className="box">
+          <h1 className="title">Welcome Back!</h1>
+          <Input id="username" refEl={username} type="text" label="Username" />
+          <Input
+            id="password"
+            refEl={password}
+            type="password"
+            label="Password"
+          />
+          <div className="field is-grouped">
+            <div className="control">
+              <button className="button is-link" onClick={submit}>
+                Login
+              </button>
+            </div>
+            <div className="control">
+              <Link href="/register">
+                <button className="button is-link is-light">Register</button>
+              </Link>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
